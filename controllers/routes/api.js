ae93a4ac86b5bc737b6a77426719e76af30e5b27
jjcourtney <jcourtneywork@gmail.com>
2021-10-27 14:01:40 +0100

    added GET for /range

--- controllers/routes/api.js
+++ controllers/routes/api.js
@@ -20,6 +20,28 @@ router.get(`/`, async (_, res) => {
 });
 
 
+
+router.get(`/range`, async (_, res) => {
+    try {
+        const dbWorkouts = await Workout.aggregate([
+            {
+                $addFields: {
+                    totalDuration: { $sum: "$exercises.duration" },
+                    totalWeight: { $sum: "$exercises.weight" },
+                },
+            },
+        ])
+            .sort({
+                day: -1
+            })
+            .limit(7);
+        res.status(200).json(dbWorkouts);
+    } catch (error) {
+        res.status(400).json(error);
+    }
+});
+
+
 router.post("/", async (req, res) => {
     try {
         const workoutToAdd = await Workout.create(req.body);
